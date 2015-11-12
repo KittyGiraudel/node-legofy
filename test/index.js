29547f0cdb6947cc730c3bbfadd59bfdfbfdfb0d
Valérian Galliat <val@codejam.info>
2015-11-12 12:03:52 -0500

    ES6 publication refactoring
    Closes #29, #30, #31.

--- test/index.js
+++ test/index.js
@@ -1,4 +1,3 @@
-import legofy from '../lib'
 import assert from 'assert'
 import fs from 'fs'
 import pipe from 'multipipe'
@@ -6,19 +5,20 @@ import digest from 'digest-stream'
 
 const expectedHash = '21a46facadf92b53c5462d24d5e0f33a7a54f318'
 
-describe('The Legofy module', () => {
-  let hash
+export default (name, legofy) =>
+  describe(`The Legofy ${name} module`, () => {
+    let hash
 
-  before(function (done) {
-    this.timeout(5000)
+    before(function (done) {
+      this.timeout(5000)
 
-    pipe(
-      fs.createReadStream('images/benchmark/sample-3.png'),
-      legofy({ format: 'png', pixelInterval: 1 }), // PNG to be deterministic
-      digest('sha1', 'hex', _hash => hash = _hash),
-      done)
-      .resume()
+      pipe(
+        fs.createReadStream('images/benchmark/sample-3.png'),
+        legofy({ format: 'png', pixelInterval: 1 }), // PNG to be deterministic
+        digest('sha1', 'hex', _hash => hash = _hash),
+        done)
+        .resume()
+    })
+
+    it('should equal expected hash', () => assert.equal(hash, expectedHash))
   })
-
-  it('should equal expected hash', () => assert.equal(hash, expectedHash))
-})
